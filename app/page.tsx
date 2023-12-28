a39d82a8c83ec67f97a2d077abe3903528a7fa27
Nik Luparev <nikita.luparev@gmail.com>
2023-12-28 22:57:19 +0500

    hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 // components
+import Image from 'next/image';
 import Header from '@/app/components/landing/header';
+
+import heroImg from '@/app/assets/hero-image.png';
 
 // styles
 import css from './page.module.css';
@@ -8,6 +11,28 @@ function Home() {
   return (
     <main>
       <Header className={css.header} />
+
+      <section className={css.heroSection}>
+        <div>
+          <h1 className={css.title}>Get ready for your next big jump</h1>
+
+          <p className={css.text}>
+            Are you ready to land your dream tech job? Unlock your full
+            potential with us, the ultimate destination for mastering tech
+            interviews and acing your career in the ever-evolving world of
+            technology.
+          </p>
+        </div>
+
+        <div>
+          <Image
+            src={heroImg}
+            alt="hero image section"
+            width={520}
+            height={346}
+          />
+        </div>
+      </section>
     </main>
   );
 }
