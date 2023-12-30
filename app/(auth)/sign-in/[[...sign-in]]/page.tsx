f6a791038555564a0bce1d468866083df07db27d
Nik Luparev <nikita.luparev@gmail.com>
2023-12-30 22:51:27 +0500

    add back buttons

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,4 +1,8 @@
 import { SignIn } from '@clerk/nextjs';
+import Link from 'next/link';
+
+// ui
+import Button from '@/app/components/ui/button';
 
 // styles
 import css from './styles.module.scss';
@@ -6,6 +10,14 @@ import css from './styles.module.scss';
 function Page() {
   return (
     <div className={css.wrapper}>
+      <header>
+        <Link href="/">
+          <Button styleType="secondary" className={css.backButton}>
+            Go back
+          </Button>
+        </Link>
+      </header>
+
       <SignIn />
     </div>
   );
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,11 +1,24 @@
 import { SignUp } from '@clerk/nextjs';
 
+import Link from 'next/link';
+
+// ui
+import Button from '@/app/components/ui/button';
+
 // styles
 import css from './styles.module.scss';
 
 function Page() {
   return (
     <div className={css.wrapper}>
+      <header>
+        <Link href="/sign-up">
+          <Button styleType="secondary" className={css.backButton}>
+            Go back
+          </Button>
+        </Link>
+      </header>
+
       <SignUp
         path="/sign-up"
         routing="path"
