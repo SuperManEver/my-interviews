7cbc5de2a60974c19c6605e5b3b01632669cb216
Nik Luparev <nikita.luparev@gmail.com>
2024-01-09 16:25:44 +0500

    adjust

--- app/components/dashboard/link-item/index.tsx
+++ app/components/dashboard/link-item/index.tsx
@@ -8,17 +8,22 @@ interface IProps {
   name: string;
   href: string;
   Icon?: any;
+  className?: string;
 }
 
-function LinkItem({ name, href, Icon }: IProps) {
+function LinkItem({ name, href, Icon, className }: IProps) {
   const pathname = usePathname();
 
   return (
     <Link href={href}>
       <div
-        className={cn(css.root, {
-          [css.active]: href === pathname,
-        })}
+        className={cn(
+          css.root,
+          {
+            [css.active]: href === pathname,
+          },
+          className,
+        )}
       >
         {Icon ? <Icon className={css.icon} /> : null}
         <p className={css.name}>{name}</p>
